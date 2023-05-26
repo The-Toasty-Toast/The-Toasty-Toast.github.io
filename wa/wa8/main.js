192a7939e6d39ae37adadd78c9442e91ac3e8ef4
Sophie Berry <sobe6933@colorado.edu>
2023-05-26 13:57:50 -0600

    new name

--- wa/wa8/main.js
+++ wa/wa8/main.js
@@ -1,4 +1,4 @@
-const customName = document.getElementById('chname');
+const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const storyText = 'It was 30 minute hike back to the main trail, so :insertx: continued on up the mountain determined to summit that afternoon. When they got to :inserty:, they  decided to take a quick break. Eating a quick snack and having afew sips of water. Ater the short break they continued on. More and more walking and then :insertz: That did not deter :insertx: and they continued up the mountain. Eventualy content but tired :insertx: reached the top of the mountain and was able to see the far away dots of trees and rocks below.';
+const insertX = ['Sam the subjectivly interested', 'Tom the beloved by some', 'Larry the long time old freind'];
+const insertY = ['a nice flat lookout point', 'a large boulder', 'a steep section covered in snow'];
+const insertZ = ['dark clouds drifted overhead and large drops of rain began to fall.', 'from above rocks shifted causing them to tumble down the side of the mountain.', 'a large snake slithered out of the rocks and blocked the path.'];
 
 randomize.addEventListener('click', result);
 
@@ -25,16 +25,12 @@ function result() {
   newStory = newStory.replaceAll(':inserty:',yItem);
   newStory = newStory.replaceAll(':insertz:',zItem);
 
-  if (customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replaceAll('Bob', name);
-  }
 
-  if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300*0.0714286)} stone`;
-    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
-    newStory = newStory.replaceAll('94 fahrenheit', temperature);
-    newStory = newStory.replaceAll('300 pounds', weight);
+  if (document.getElementById("distance").checked) {
+    const minutes = `30 minutes`;
+    const  meters=  `48285 meters`;
+    newStory = newStory.replaceAll('30 minute', minutes);
+    newStory = newStory.replaceAll('48285', meters);
   }
 
   story.textContent = newStory;
