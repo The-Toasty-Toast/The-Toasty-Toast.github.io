5fd99cc6bca30687200781b28421c67c668c47d0
Sophie Berry <sobe6933@colorado.edu>
2023-05-26 10:58:15 -0600

    picture change

--- ica/ica8/ica8.js
+++ ica/ica8/ica8.js
@@ -5,3 +5,13 @@ if (tname != null) {
   document.getElementById("enter-name").innerHTML =
   "The name of the toucan is now " + tname +"!"}
 }
+
+function toucan(sw) {
+    var pic;
+    if (sw == 0) {
+      pic = "toucan.png"
+    } else {
+      pic = "toucan2.png"
+    }
+    document.getElementById('myImage').src = pic;
+  }
