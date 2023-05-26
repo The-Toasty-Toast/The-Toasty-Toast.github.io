2a5fa33699cf40daf25715187cfc84c0308f3b83
Sophie Berry <sobe6933@colorado.edu>
2023-05-26 10:30:13 -0600

    picture 2

--- ica/ica8/ica8.js
+++ ica/ica8/ica8.js
@@ -1,3 +1,7 @@
-<script>
-alert(5 + 6);
-</script>
+
+function myFunction() {
+let tname = prompt("What would you name the Toucan?");
+if (tname != null) {
+  document.getElementById("enter-name").innerHTML =
+  "The name of the toucan is now " + tname +"!"}
+}
