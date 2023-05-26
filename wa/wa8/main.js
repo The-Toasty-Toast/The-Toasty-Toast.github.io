9013219677e3057c3c5ff5c7ffbd196c7bcb971d
Sophie Berry <sobe6933@colorado.edu>
2023-05-26 15:06:20 -0600

    minutes to distance

--- wa/wa8/main.js
+++ wa/wa8/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 30 minute hike back to the main trail, so :insertx: continued on up the mountain determined to summit that afternoon. When they got to :inserty:, they  decided to take a quick break. Eating a quick snack and having afew sips of water. Ater the short break they continued on. More and more walking and then :insertz: That did not deter :insertx: and they continued up the mountain. Eventualy content but tired :insertx: reached the top of the mountain and was able to see the far away dots of trees and rocks below.';
+const storyText = 'It was 48285 meter hike back to the main trail, so :insertx: continued on up the mountain determined to summit that afternoon. When they got to :inserty:, they  decided to take a quick break. Eating a quick snack and having afew sips of water. Ater the short break they continued on. More and more walking and then :insertz: That did not deter :insertx: and they continued up the mountain. Eventualy content but tired :insertx: reached the top of the mountain and was able to see the far away dots of trees and rocks below.';
 const insertX = ['Sam the subjectivly interested', 'Tom the beloved by some', 'Larry the long time old freind'];
 const insertY = ['a nice flat lookout point', 'a large boulder', 'a steep section covered in snow'];
 const insertZ = ['dark clouds drifted overhead and large drops of rain began to fall.', 'from above rocks shifted causing them to tumble down the side of the mountain.', 'a large snake slithered out of the rocks and blocked the path.'];
@@ -30,10 +30,9 @@ function result() {
     newStory = newStory.replaceAll('walk', name);
   }
 
-  if (document.getElementById("meter").checked) {
-    
-    newStory = newStory.replaceAll('48285 meter', meter);
-    newStory = newStory.replaceAll('30 minute', minute);
+  if (document.getElementById("minute").checked) {
+    newStory = newStory.replaceAll('48285 meter', '30 minute');
+   
   
   }
 
