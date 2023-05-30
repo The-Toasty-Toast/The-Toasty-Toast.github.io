eeead4b370bec9d3c9ed41cb218d8f3f5fd54a0c
Sophie Berry <sobe6933@colorado.edu>
2023-05-30 14:38:01 -0600

    adding new files and editing the ICA9

---
+++ ica/ICA9/ICA9.js
@@ -0,0 +1,48 @@
+const button= document.
+querySelector('#js-new-quote');
+
+// checking that this is working 
+//onsole.log(button)
+
+const questionBtnClick= questionButton.addEventListener
+('click', newTrivia);
+const answerBtnClick= document
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+function newQuestion(){
+    const output = newTrivia();
+    console.log(output);
+}
+
+async function newTrivia(){
+    //console.log("button pressed!");
+    try{
+        const response = await fetch(endpoint);
+        if(!responce.ok){
+            throw Error(responce.statusText)
+        }
+
+
+   // console.log(responce);
+        const jsonData = await response.json();
+        const quoteTest=jsonData['question'];
+        const quoteArea = document.querySelector('#js-quote-text');
+        quoteArea.textConst= quoteText;
+        //console.log(jsonData ['question']);
+       //console.log(jsonData ['answer']);}
+    }
+    catch (err){
+        console.log(err)
+        alert ('Failed');
+    
+    }
+
+
+
+function giveAnswer(){
+    const answerText = jsonData['answer'];
+    const answerArea = document.
+}
+}
+
+
