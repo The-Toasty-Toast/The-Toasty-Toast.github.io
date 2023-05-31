88df528d52e312ce8df05d595f5a263568a458ac
Sophie Berry <sobe6933@colorado.edu>
2023-05-31 17:37:27 -0600

    Update WA10.js

--- wa/WA10.js
+++ wa/WA10.js
@@ -2,10 +2,10 @@
        //Problem 1
        //Problem3
    let data=[
-      {"firstName":"Sam", "department":"Tech", "designation":"Manager", "salary":"40000", "raise eligible":"true"},
-      {"firstName":"Mary", "department":"Finance", "designation":"Trainee", "salary":"18500", "raise eligible":"true"},
-      {"firstName":"Bill", "department":"HR", "designation":"Executive", "salary":"21200", "raise eligible":"false"}, 
-      {"firstName":"Anna", "department":"Tech", "designation":"Executive", "salary":"25600", "raise eligible":"false"}, 
+      {"firstName":"Sam", "department":"Tech", "designation":"Manager", "salary":40000, "raise eligible":"true"},
+      {"firstName":"Mary", "department":"Finance", "designation":"Trainee", "salary":18500, "raise eligible":"true"},
+      {"firstName":"Bill", "department":"HR", "designation":"Executive", "salary":21200, "raise eligible":"false"}, 
+      {"firstName":"Anna", "department":"Tech", "designation":"Executive", "salary":25600, "raise eligible":"false"}, 
     ]
 
     console.log("Problem 1");
@@ -19,6 +19,7 @@
     console.log(data[1]);
     console.log(data[2]);
     console.log(data[3]);
+    
 
 //Problem 2
     let company =[
@@ -29,33 +30,46 @@
     console.log(company);
 
  //Problem 4
- /*
-  SUM(salary) 
-    form data;
-    for (let i = 0; i < 3; i++)
-       salaryTotal = data [0] [salery]+ data [1] [salery];
+ let  salaryTotal = 0;
+    for (let i = 0; i < data.length; i++)
+       salaryTotal += Number(data [i]['salary']);
      
     console.log("Problem 4");
-     console.log(data);
+     console.log(salaryTotal);
 
-     for (let data[0]; data <data[3]; i++)*/
-      //Problem 5
-for 
-     if (data ["raise eligible"] == true) 
-     Raise = (salery * 0.1)+ salery; 
-     set data ["raise eligible"] = False;
+
+    //Problem 5
+    for (let i = 0; i < data.length; i++){
+
+     if (data[i] ['raise eligible'] == "true") {
+     let raise = (data[i] ['salary'] * 1.1);
+     data[i] ['salary'] = raise;
+     data[i] ['raise eligible'] = "false";
+     }
+    }
+    console.log("Problem 5");
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
+    console.log(data[3]);
+
 
      //Problem 6
+     let wfh=['Anna', 'Sam'];
+        
 
-
-
-for
-
- if sam or sam == in work from home 
-
-if data == work from home =1 
-wfh 
-set true 
-
-else set work from home =false 
-wfh.includes (false)
+    for (let i = 0; i < data.length; i++){
+        
+        
+        if (wfh.includes(data[i]['firstName'])) {
+            data[i] ['wfh'] = "true";   
+        }
+        else
+            data[i] ['wfh'] = "false";  
+    
+    }
+    console.log("Problem 6");
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
+    console.log(data[3]);
