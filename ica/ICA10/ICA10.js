0e80c5abe514332d79a133bf489b447075b7913f
Sophie Berry <sobe6933@colorado.edu>
2023-05-31 13:42:39 -0600

    new folder and adding information

--- ica/ICA10/ICA10.js
+++ ica/ICA10/ICA10.js
@@ -18,3 +18,20 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
