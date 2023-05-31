81bcc887e68f29c6bd536ae18ecbe86e5e3c086e
Sophie Berry <sobe6933@colorado.edu>
2023-05-31 13:54:52 -0600

    moving the ball

--- ica/ICA10/ICA10.js
+++ ica/ICA10/ICA10.js
@@ -34,4 +34,26 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
+
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
 }
---
+++ wa/WA10.js
@@ -0,0 +1,7 @@
+{
+    "employees":[
+      {"firstName":"John", "department":"Doe", "department":"Doe", "department":"Doe", "department":"Doe"},
+      {"firstName":"Anna", "lastName":"Smith"},
+      {"firstName":"Peter", "lastName":"Jones"}
+    ]
+    }
