9f570b6f3cf9c29b8b3152dcc29a0e003378d69c
Sophie Berry <sobe6933@colorado.edu>
2023-05-31 15:24:18 -0600

    Update WA10.js

--- wa/WA10.js
+++ wa/WA10.js
@@ -1,20 +1,37 @@
 
-    let data=[
+       //Problem 1
+       //Problem3
+   let data=[
       {"firstName":"Sam", "department":"Tech", "designation":"Manager", "salary":"40000", "raise eligible":"true"},
       {"firstName":"Mary", "department":"Finance", "designation":"Trainee", "salary":"18500", "raise eligible":"true"},
       {"firstName":"Bill", "department":"HR", "designation":"Executive", "salary":"21200", "raise eligible":"false"}, 
       {"firstName":"Anna", "department":"Tech", "designation":"Executive", "salary":"25600", "raise eligible":"false"}, 
     ]
-    console.log("Problem 1");
-    console.log(data);
 
+    console.log("Problem 1");
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
+    
+
+    console.log("Problem 3");
+    console.log(data[0]);
+    console.log(data[1]);
+    console.log(data[2]);
+    console.log(data[3]);
+
+//Problem 2
     let company =[
         {"companyName":"Tech Stars", "website":"www.techstars.site", "employees":"array of Employees",}
       ]
 
     console.log("Problem 2");
     console.log(company);
-    
-    for (let data[0]; data [i]; i++) {
-        salaryTotal = data [0] [salery]+ data [1] [salery];
-      }
+
+ //Problem 4
+   /* for (let i = 0; i < 3; i++)
+       salaryTotal = data [0] [salery]+ data [1] [salery];
+     }
+    console.log("Problem 4");
+     console.log(data);
+   */
