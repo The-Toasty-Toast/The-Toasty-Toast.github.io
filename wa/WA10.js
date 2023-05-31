10e3d95c040a32c6caa9ec3c125c8dd6190cd0d1
Sophie Berry <sobe6933@colorado.edu>
2023-05-31 14:36:23 -0600

    ICA finishing up and js for the wa

--- wa/WA10.js
+++ wa/WA10.js
@@ -1,7 +1,20 @@
-{
-    "employees":[
-      {"firstName":"John", "department":"Doe", "department":"Doe", "department":"Doe", "department":"Doe"},
-      {"firstName":"Anna", "lastName":"Smith"},
-      {"firstName":"Peter", "lastName":"Jones"}
+
+    let data=[
+      {"firstName":"Sam", "department":"Tech", "designation":"Manager", "salary":"40000", "raise eligible":"true"},
+      {"firstName":"Mary", "department":"Finance", "designation":"Trainee", "salary":"18500", "raise eligible":"true"},
+      {"firstName":"Bill", "department":"HR", "designation":"Executive", "salary":"21200", "raise eligible":"false"}, 
+      {"firstName":"Anna", "department":"Tech", "designation":"Executive", "salary":"25600", "raise eligible":"false"}, 
     ]
-    }
+    console.log("Problem 1");
+    console.log(data);
+
+    let company =[
+        {"companyName":"Tech Stars", "website":"www.techstars.site", "employees":"array of Employees",}
+      ]
+
+    console.log("Problem 2");
+    console.log(company);
+    
+    for (let data[0]; data [i]; i++) {
+        salaryTotal = data [0] [salery]+ data [1] [salery];
+      }
