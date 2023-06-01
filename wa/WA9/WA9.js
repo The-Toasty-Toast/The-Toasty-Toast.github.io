fb8b4f4ff1ec957ee0aab326a8669f3d91af9071
Sophie Berry <sobe6933@colorado.edu>
2023-06-01 09:07:29 -0600

    add

--- wa/WA9/WA9.js
+++ wa/WA9/WA9.js
@@ -1,14 +1,23 @@
+const button = document.querySelector ('#Fox-image');
+const BtnClick = button.addEventListener('click', newImage);
 
+const endpoint = "https://randomfox.ca/floof/?ref=apilist.fun";
 
-myVar = document.getElementById("demo");
+async function newImage() {
+       // const  = document.querySelector('#Fox-image');
+        //answerArea.imageContent='';
+    var response = await fetch(endpoint);
+    if(!response.ok){
+        throw Error(response.statusImage)
+    }
+console.log(statusImage)
+    jsonData = await response.json();
+    const imageF = jsonData['image'];
+    const imageArea = document.querySelector('#Fox-image');
+    imageArea.imageContent= imageF;  
 
-function getImage() {
-  try {<button onclick="js-new-image(https://randomfox.ca/floof/?ref=apilist.fun)">Generate a new photo!</button>
-
-    
-varLink = (myVar.attributes.href);
-  } catch {
-    myVar = err;
-  }
+catch (err){
+    console.log(err)
+    alert ('Failed');
 }
-
+}
